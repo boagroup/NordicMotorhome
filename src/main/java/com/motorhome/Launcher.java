0df0b48dfc520a690c845b7b32499302b9954651
Octavian-Roman <work.octavian@gmail.com>
2022-05-02 17:36:18 +0200

    Adding documentation for Launcher class

--- src/main/java/com/motorhome/Launcher.java
+++ src/main/java/com/motorhome/Launcher.java
@@ -1,5 +1,9 @@
 package com.motorhome;
 
+/**
+ * Entry point
+ * Needed to obtain a working artifact for JavaFX Applications
+ */
 public class Launcher {
     public static void main(String[] args) {
         Main.main(args);
