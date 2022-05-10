87f988a5b16503ff401ce35e4c8bd95ad5863c77
bart0709 <bart0709@stud.kea.dk>
2022-05-10 21:18:04 +0200

    updated scripts/ddl.sql

--- scripts/ddl.sql
+++ scripts/ddl.sql
@@ -1,13 +1,15 @@
 /*
   Script responsible for generating the SQL tables required for the back-end of the application
-  Author(s): Octavian Roman
+  Author(s): Octavian Roman, Bartosz Birylo
  */
 USE heroku_e8f7f82549e360a;
 
+/* staff and users */
+
 CREATE TABLE IF NOT EXISTS staff (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    firstName VARCHAR(20) NOT NULL,
-    lastName VARCHAR(20),
+    firstName VARCHAR(255) NOT NULL,
+    lastName VARCHAR(255),
     image VARCHAR(255) DEFAULT '/assets/user_placeholder.png',
     telephone VARCHAR(16),
     role VARCHAR(25),
@@ -23,34 +25,44 @@ CREATE TABLE IF NOT EXISTS staff (
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     staff_id INT NOT NULL,
-    username VARCHAR(16) UNIQUE NOT NULL,
-    password VARCHAR(32) NOT NULL,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
     admin BOOLEAN NOT NULL,
     FOREIGN KEY (staff_id) REFERENCES staff(id) ON DELETE CASCADE
 );
 
+/* motorhomes and its attributes */
+
 CREATE TABLE IF NOT EXISTS motorhomes (
     id INT AUTO_INCREMENT PRIMARY KEY,
     image VARCHAR(255) DEFAULT '/assets/motorhome_placeholder.png',
-    type VARCHAR(30),
-    beds INT(2)
+    model_id INT,
+    type VARCHAR(255) NOT NULL,
+    beds INT(2),
+    FOREIGN KEY (model_id) REFERENCES models(id) ON DELETE SET NULL,
+    FOREIGN KEY (beds) REFERENCES beds(amount) ON DELETE SET NULL
 );
 
 CREATE TABLE IF NOT EXISTS brands (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    motorhome_id INT,
-    name VARCHAR(25),
-    price FLOAT(10),
-    FOREIGN KEY (motorhome_id) REFERENCES motorhomes(id)
+    name VARCHAR(255),
+    price DOUBLE(10,2)
 );
 
 CREATE TABLE IF NOT EXISTS models (
     id INT AUTO_INCREMENT PRIMARY KEY,
     brand_id INT,
-    name VARCHAR(25),
-    price FLOAT(10),
-    FOREIGN KEY (brand_id) REFERENCES brands(id) ON DELETE CASCADE
+    name VARCHAR(255),
+    price DOUBLE(10,2),
+    FOREIGN KEY (brand_id) REFERENCES brands(id) ON DELETE SET NULL
 );
+
+CREATE TABLE IF NOT EXISTS beds (
+    amount INT PRIMARY KEY,
+    price DOUBLE(10,2)
+);
+
+/* rentals and its attributes */
 
 CREATE TABLE IF NOT EXISTS rentals (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -71,22 +83,23 @@ CREATE TABLE IF NOT EXISTS rentals (
     start_date DATE NOT NULL,
     end_date DATE NOT NULL,
     notes TEXT,
-    FOREIGN KEY (motorhome_id) REFERENCES motorhomes(id) ON DELETE CASCADE
+    FOREIGN KEY (motorhome_id) REFERENCES motorhomes(id) ON DELETE SET NULL
 );
 
 CREATE TABLE IF NOT EXISTS extras (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255)
+    name VARCHAR(255),
+    price DOUBLE(10,2)
 );
 
 CREATE TABLE IF NOT EXISTS rentalExtras (
-    id INT AUTO_INCREMENT PRIMARY KEY,
     rental_id INT,
     extra_id INT,
-    price FLOAT,
-    FOREIGN KEY (rental_id) REFERENCES rentals(id),
-    FOREIGN KEY (extra_id) REFERENCES extras(id)
+    FOREIGN KEY (rental_id) REFERENCES rentals(id) ON DELETE CASCADE,
+    FOREIGN KEY (extra_id) REFERENCES extras(id) ON DELETE CASCADE
 );
+
+/* clients and its attributes */
 
 CREATE TABLE IF NOT EXISTS clients (
     id INT AUTO_INCREMENT PRIMARY KEY,
