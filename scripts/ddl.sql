d663176e9308fa9ef0e0c450b776c23aee273d56
Octavian-Roman <work.octavian@gmail.com>
2022-05-11 00:53:47 +0200

    ddl fix
    bart pls

--- scripts/ddl.sql
+++ scripts/ddl.sql
@@ -26,7 +26,7 @@ CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     staff_id INT NOT NULL,
     username VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
+    password BLOB NOT NULL,
     admin BOOLEAN NOT NULL,
     FOREIGN KEY (staff_id) REFERENCES staff(id) ON DELETE CASCADE
 );
