ed0eefd22ffa312c53e9fc2ebb42f97fc97d93a6
bart0709 <bart0709@stud.kea.dk>
2022-05-11 17:21:39 +0200

    added documentation to Database class

--- src/main/java/com/motorhome/database/Database.java
+++ src/main/java/com/motorhome/database/Database.java
@@ -4,7 +4,7 @@ import java.sql.*;
 
 /**
  * Contains simple methods to connect to the database
- * Author(s): Octavian Roman
+ * Author(s): Bartosz Birylo, Octavian Roman
  */
 public final class Database {
     private static Database instance;
@@ -15,7 +15,11 @@ public final class Database {
 
     private static Connection connection = null;
 
-
+    /**
+     * singleton pattern lazy method to get Instance of the singleton object
+     * if the singleton was already created with other paremeters or the default ones, new instance will not be created
+     * @return {@code Database} instance
+     */
     public static Database getInstance() {
         if (instance == null) {
             instance = new Database();
@@ -23,6 +27,14 @@ public final class Database {
         return instance;
     }
 
+    /**
+     * singleton pattern lazy method to get Instance of the singleton object
+     * if the singleton was already created with other paremeters or the default ones, new instance will not be created
+     * @param user {@date String} - database user
+     * @param password {@date String} - database user's password
+     * @param url {@date String} - database url
+     * @return {@code Database} instance
+     */
     public static Database getInstance(String user, String password, String url) {
         if (instance == null) {
             instance = new Database(user, password, url);
@@ -30,18 +42,28 @@ public final class Database {
         return instance;
     }
 
+    /**
+     * private constructor to keep it singleton
+     */
     private Database() {
         this.user = System.getProperty("user");
         this.password = System.getProperty("password");
         this.url = "jdbc:mysql://eu-cdbr-west-02.cleardb.net:3306/heroku_e8f7f82549e360a?reconnect=true";
     }
 
+    /**
+     * private constructor to keep it singleton
+     */
     private Database(String user, String password, String url) {
         this.user = user;
         this.password = password;
         this.url = url;
     }
 
+    /**
+     * method to connect to the database
+     * @return {@code true} if successful, else {@code false}
+     */
     private boolean connect() {
         try {
             if (connection == null || !connection.isValid(1000)) {
@@ -55,6 +77,10 @@ public final class Database {
         return false;
     }
 
+    /**
+     * method to close the connection to the database
+     * @return {@code true} if successful, else {@code false}
+     */
     private boolean close() {
         if (connection != null) {
             try {
@@ -68,6 +94,12 @@ public final class Database {
         return true;
     }
 
+    /**
+     * method that executes SQL query in the database and returns the result as {@code DataResult}
+     * @param query {@code String} - string representation of SQL query
+     * @param parameters {@code String...} - strings that will be interpolated into SQL query
+     * @return {@code DataResult} - custom representation of results from the query as columns and rows
+     */
     public DataResult executeQuery(String query, String... parameters) {
         try {
             if (connect()) {
@@ -87,8 +119,10 @@ public final class Database {
 
     /**
      * Establish a connection to our Heroku ClearDB remote database
+     * deprecated, kept for compatibility
      * @return the connection
      */
+    @Deprecated
     public Connection getConnection() {
         try {
             return DriverManager.getConnection(url, user, password);
@@ -100,8 +134,10 @@ public final class Database {
 
     /**
      * Closes the connection the remote database
+     * deprecated, kept for compatibility
      * @param connection the connection to be closed
      */
+    @Deprecated
     public void closeConnection(Connection connection) {
         if (connection != null) {
             try {
