05eb4c6f1a78caac884dced7d588e58c801bab8c
bart0709 <bart0709@stud.kea.dk>
2022-05-15 02:37:02 +0200

    added BaseModel, Motorhome models

---
+++ src/main/java/com/motorhome/model/BaseModel.java
@@ -0,0 +1,3 @@
+package com.motorhome.model;
+
+public interface BaseModel {}
---
+++ src/main/java/com/motorhome/model/Motorhome.java
@@ -0,0 +1,61 @@
+package com.motorhome.model;
+
+import com.motorhome.persistence.DataResult;
+
+import java.util.Map;
+
+public class Motorhome implements BaseModel {
+	private int id;
+	private String image;
+	private String type;
+	private int beds;
+
+	public Motorhome(Map<String, Object> row) {
+		this.id = (int) row.get("id");
+		this.image = (String) row.get("image");
+		this.type = (String) row.get("type");
+		this.beds = (int) row.get("beds");
+	}
+
+	public Motorhome(int id, String image, String type, int beds) {
+		this.id = id;
+		this.image = image;
+		this.type = type;
+		this.beds = beds;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public int getBeds() {
+		return beds;
+	}
+
+	public void setBeds(int beds) {
+		this.beds = beds;
+	}
+
+
+
+}
